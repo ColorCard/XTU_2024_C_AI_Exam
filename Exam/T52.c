bb67dffa9e29399f678e6909ee1e72928517a734
yestan1125 <183602647+yestan1125@users.noreply.github.com>
2024-12-24 11:54:14 +0800

    Update T52.c

--- Exam/T52.c
+++ Exam/T52.c
@@ -2,6 +2,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int n;
+    scanf("%d", &n);
+    int sum = 1; 
+
+    for (int i = n - 1; i >= 1; i--) {
+        sum = (sum + 1) * 2;  // 根据每天吃桃的规律倒推前一天桃子的数量
+    }
+
+    printf("%d\n", sum);
+
     return 0;
 }
